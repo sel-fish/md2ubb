8bb4c2698f90ccdb776f321a07bc6694cc2abaea
sel-fish <fqlgy@hotmail.com>
2017-05-31 00:05:56 +0800

    add image support ..

--- renderer.py
+++ renderer.py
@@ -125,6 +125,20 @@ class UbbRenderer(object):
         """Rendering list item snippet. Like ``<li>``."""
         return '[*]%s\n' % text
 
+    def image(self, src, title, text):
+        """Rendering a image with title and text.
+
+        :param src: source link of the image.
+        :param title: title text of the image.
+        :param text: alt text of the image.
+        """
+        src = escape_link(src)
+        html = '[img]%s[/img]' % src
+        if self.options.get('use_xhtml'):
+            return '%s' % html
+        return '%s' % html
+
+
 class HtmlRenderer(object):
     """The default HTML renderer for rendering Markdown.
     Copy from mistune ..
